de6062a2731c9d3b1b896d20cbaecce6bb8af395
https-sharif <sharif.islam96403@gmail.com>
2025-09-16 02:07:17 +0600

    Added unsupervised learning algorithm and pseudo labelling to csv

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-r = requests.get("https://api.github.com/rate_limit", headers={"Authorization": f"token {GITHUB_TOKEN}"})
+headers = {"Authorization": f"Bearer {GITHUB_TOKEN}"}
+
+r = requests.get("https://api.github.com/rate_limit", headers=headers)
 
 print("Status code:", r.status_code)
 print("Response text:", r.text)
